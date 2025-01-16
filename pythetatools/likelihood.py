f0c6718f0ab966377a1aebaabfbf0bd36730142c
dcarabadjac <dcarabadjac@llr.in2p3.fr>
2025-01-16 12:55:40 +0200

    Improved the way to find minimal value of the llh

--- pythetatools/likelihood.py
+++ pythetatools/likelihood.py
@@ -7,6 +7,8 @@ from .base_analysis import *
 import sys
 import glob
 import pandas as pd
+from scipy.interpolate import interp1d, RectBivariateSpline
+from scipy.optimize import minimize
 
 
 def load(file_pattern, nthrows_per_file, target_nthrows, mo="both"):
@@ -107,11 +109,27 @@ def load(file_pattern, nthrows_per_file, target_nthrows, mo="both"):
 
     return grid, AvNLLtot, param_name
 
-
 class loglikelihood:
+    """
+    A class to compute and visualize 1D or 2D log-likelihood and Δχ² for oscillation parameter analyses.
+    
+    Attributes:
+    -----------
+    grid : list of numpy.ndarray
+        Parameter grids for the likelihood analysis.
+    avnllh : dict
+        A dictionary containing AvNLLH values for different mass orderings (keys are 0, 1, or 'both').
+    param_name : list of str
+        Names of the oscillation parameters corresponding to the grid.
+    dchi2 : dict
+        Dictionary of Δχ² values for each mass ordering.
+    mo : str or int
+        Indicates the tested mass ordering hypotheses ('both', 0, or 1).
+    """
+    
     def __init__(self, grid, avnllh, param_name, kind='joint'):
         """
-        Initialize the object with grid, AvNLLH values, and parameter names.
+        Initialize the object.
     
         Parameters:
         -----------
@@ -135,66 +153,160 @@ class loglikelihood:
         if kind not in ['joint', 'conditional']:
             raise ValueError("Invalid kind: choose 'joint' or 'conditional'.")
             
-        # Perform swapping of osc params if necessary to standardize the osc. params axes for plotting
+        # Perform swapping of oscillation parameters if necessary to standardize the axes
         if param_name == ['dm2', 'sin223']:
             self.__swap_grid_and_param()
+
+        valid_mo_keys = {0, 1, 'both'}
+        if not set(self.__avnllh.keys()).issubset(valid_mo_keys):
+            raise ValueError(f"Invalid keys in avnllh. Expected keys: {valid_mo_keys}")
             
         self.__mo = 'both' if len(self.__avnllh.keys()) == 2 else list(self.__avnllh.keys())[0]
         self.__dchi2 = self.__calculate_dchi2(kind)
 
+    def __find_minimum(self, array):
+        def calculate_x0(bounds):
+            return [(bound[1] + bound[0])/2 for bound in bounds]
+            
+        if self.ndim() == 1:
+            interpolator = interp1d(self.__grid[0], array, kind='linear')
+            bounds = [(self.__grid[0][0], self.__grid[0][-1])]
+            x0 = self.grid[0][np.argmin(array)]
+            result = minimize(interpolator, x0=x0, bounds=bounds)
+            self.__min = result.x
+            return result.fun
+    
+        elif self.ndim() == 2:
+            interpolator = RectBivariateSpline(self.__grid[0], self.__grid[1], array)
+            # Define the function to minimize
+            def func_to_minimize(xy):
+                xi, yi = xy
+                return interpolator(xi, yi)[0, 0] 
+            bounds = [(self.__grid[0][0], self.__grid[0][-1]), (self.__grid[1][0], self.__grid[1][-1])]
+            x0 = calculate_x0(bounds)
+            result = minimize(func_to_minimize, x0=x0, bounds=bounds)
+            self.__min = result.x
+            return result.fun
+    
+    @property
+    def grid(self):
+        """Get the parameter grid."""
+        return self.__grid
+    
+    @property
+    def avnllh(self):
+        """Get the AvNLLH values."""
+        return self.__avnllh
+
+    @property
+    def param_name(self):
+        """Get the parameter names."""
+        return self.__param_name
+    
+    @property
+    def dchi2(self):
+        """Get the Δχ² values."""
+        return self.__dchi2
+
+    @property
+    def mo(self):
+        """Get the tested mass ordering hypotheses."""
+        return self.__mo
+
     def __swap_grid_and_param(self):
         """
-        Swap grid and parameter names to standardize the osc. params axes for plotting.
+        Swap grid and parameter names to standardize the parameter axes for plotting.
+        This is applied when the grid order is unconventional (e.g., (X,Y) = ['dm2', 'sin223'] instead of ['dm2', 'sin223']).
         """
-        # Swap elements in grid and parameter names
         self.__grid[0], self.__grid[1] = self.__grid[1], self.__grid[0]
         self.__param_name[0], self.__param_name[1] = self.__param_name[1], self.__param_name[0]
     
-        # Transpose the AvNLLH arrays
         for key in self.__avnllh.keys():
             self.__avnllh[key] = self.__avnllh[key].transpose()
     
     def __calculate_dchi2(self, kind):
         """
-        Calculate the Δχ² values
-    
+        Calculate the Δχ² values for the likelihood analysis.
+
         Parameters:
         -----------
         kind : str
             Type of likelihood analysis ('joint' or 'conditional').
-    
+
         Returns:
         --------
         dchi2 : dict
             Dictionary of Δχ² values for each mass ordering.
-
         """
         if kind == 'joint':
             # Joint minimization: find the global minimum across all mass orderings
-            global_minimum = min(np.min(array) for array in self.__avnllh.values())
-            dchi2 = {key: 2 * (value - global_minimum) for key, value in self.__avnllh.items()}
+            global_minimum = min(self.__find_minimum(array) for array in self.__avnllh.values())
+            dchi2 = {key: 2 * (avnllh - global_minimum) for key, avnllh in self.__avnllh.items()}
         elif kind == 'conditional':
             # Conditional minimization: find the minimum for each mass ordering
-            dchi2 = {key: 2 * (value - np.min(value)) for key, value in self.__avnllh.items()}
-    
+            dchi2 = {key: 2 * (avnllh - self.__find_minimum(avnllh)) for key, avnllh in self.__avnllh.items()}
+        
         return dchi2
 
     def ndim(self):
+        """
+        Get the dimensionality of the parameter grid.
+
+        Returns:
+        --------
+        int
+            The number of dimensions in the grid.
+        """
         return len(self.__grid)
 
     def plot(self, ax, wtag=False, mo=None, show_legend=True, show_map=False, cls=['1sigma', '90%', '3sigma'], **kwargs):
+        """
+        Plot the Δχ² values in 1D or 2D.
+
+        Parameters:
+        -----------
+        ax : matplotlib.axes.Axes
+            The axis to plot on.
+        wtag : bool, optional
+            If True, include a tag in the plot. Default is False.
+        mo : int or str, optional
+            Mass ordering to plot. Default is None (all mass orderings).
+        show_legend : bool, optional
+            If True, show the legend. Default is True.
+        show_map : bool, optional
+            If True, show the color map in 2D plots. Default is False.
+        cls : list of str, optional
+            Confidence levels to plot. Default is ['1sigma', '90%', '3sigma'].
+        kwargs : dict
+            Additional arguments for plotting.
+        """
         if self.ndim() == 1:
             self._plot_1d(ax, wtag, mo, show_legend, **kwargs)
         elif self.ndim() == 2:
             self._plot_2d(ax, wtag, mo, show_map, cls, **kwargs)
         else:
             raise ValueError(f"Plotting is only supported for 1D and 2D delta chi2. But the llh dimension is {self.ndim()}")     
-
+    
     def find_CI(self, nsigma, mo):
+        """
+        Find confidence intervals at a given significance level.
+    
+        Parameters:
+        -----------
+        nsigma : float
+            The number of standard deviations for the confidence level.
+        mo : int or str
+            The mass ordering to consider ('0', '1', or 'both').
+    
+        Raises:
+        -------
+        ValueError
+            If the dimensionality of the data is not 1D or 2D.
+        """
         if self.ndim() == 1:
-            self._find_CI_1d(nsigma, mo)
+            return self._find_CI_1d(nsigma, mo)
         elif self.ndim() == 2:
-            self._find_CI_2d(nsigma, mo)
+            return self._find_CI_2d(nsigma, mo)
         else:
             raise ValueError(f"find_CI is only supported for 1D and 2D delta chi2. But the llh dimension is {self.ndim()}")     
 
@@ -202,49 +314,28 @@ class loglikelihood:
         edges_left = []   
         edges_right = []
         c = np.sqrt(nsigma)
-
-        #Treat the case if margin points in inside C.I.
+    
+        # Treat the case if margin points are inside C.I.
         if self.dchi2[mo][0] <= c:
             edges_left.append(self.grid[0])
         if self.dchi2[mo][-1] <= c:
             edges_right.append(self.grid[-1])
-
-        #Find all the margins of C.I.
+    
+        # Find all the margins of C.I.
         for i in range(len(self.grid) - 1):
             y0, y1 = self.dchi2[mo][i], self.dchi2[mo][i + 1]
-            if (y0 - c)>=0 and (y1 - c)<=0:
+            if (y0 - c) >= 0 and (y1 - c) <= 0:
                 x0, x1 = self.grid[i], self.grid[i + 1]
                 edge_left = x0 + (x1 - x0) * (c - y0) / (y1 - y0)
                 edges_left.append(edge_left) 
-            if (y0 - c)<=0 and (y1 - c)>=0:
+            if (y0 - c) <= 0 and (y1 - c) >= 0:
                 x0, x1 = self.grid[i], self.grid[i + 1]
                 edge_right = x0 + (x1 - x0) * (c - y0) / (y1 - y0)
                 edges_right.append(edge_right)          
         return edges_left, edges_right
 
     def _find_CI_2d(self, nsigma, mo):
-        edges_left = []   
-        edges_right = []
-        c = np.sqrt(nsigma)
-
-        #Treat the case if margin points in inside C.I.
-        if self.dchi2[mo][0] <= c:
-            edges_left.append(self.grid[0])
-        if self.dchi2[mo][-1] <= c:
-            edges_right.append(self.grid[-1])
-
-        #Find all the margins of C.I.
-        for i in range(len(self.grid) - 1):
-            y0, y1 = self.dchi2[mo][i], self.dchi2[mo][i + 1]
-            if (y0 - c)>=0 and (y1 - c)<=0:
-                x0, x1 = self.grid[i], self.grid[i + 1]
-                edge_left = x0 + (x1 - x0) * (c - y0) / (y1 - y0)
-                edges_left.append(edge_left) 
-            if (y0 - c)<=0 and (y1 - c)>=0:
-                x0, x1 = self.grid[i], self.grid[i + 1]
-                edge_right = x0 + (x1 - x0) * (c - y0) / (y1 - y0)
-                edges_right.append(edge_right)          
-        return edges_left, edges_right
+        pass
 
     def _plot_1d(self, ax, wtag, mo, show_legend, **kwargs):
         if not mo is None:
@@ -280,14 +371,18 @@ class loglikelihood:
             
         if show_map and mo is not None:
             mesh = ax.pcolormesh(self.__grid[0], self.__grid[1], self.__dchi2[mo].transpose(), zorder=0,  **kwargs)
-            cbar = plt.colorbar(mesh, ax=ax)    
+            cbar = plt.colorbar(mesh, ax=ax)
+            print(self.__min)
+            plt.scatter(self.__min)
             color = 'white'
        
-        if not mo is None:
+        if mo is not None:
             if mo in self.__dchi2.keys():
                 contour = ax.contour(self.__grid[0], self.__grid[1], self.__dchi2[mo].transpose(), 
                                      levels=critical_values, colors=color, linewidths=2, 
                                      zorder=1, linestyles=['-', '--', 'dotted'])
+                print(self.__min)
+                plt.scatter(*self.__min, marker='x')
             else:
                 raise ValueError(f"There is not dchi2 with mo={mo}")  
         else:
@@ -301,6 +396,9 @@ class loglikelihood:
         ax.set_xlabel(osc_param_name_to_xlabel[self.__param_name[0]][mo])
         ax.set_ylabel(osc_param_name_to_xlabel[self.__param_name[1]][mo])
         show_minor_ticks(ax)
+
+
+
 
 
 
